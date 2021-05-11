b14b19cd3296483c082dd8502702915d22134ba8
Huber GERARD <huber.gerard1@gmail.com>
2021-05-11 09:50:45 +0200

    Update RomanToArabicTest.java
    Add test

--- RomanToArabicTest.java
+++ RomanToArabicTest.java
@@ -6,4 +6,13 @@ public class RomanToArabicTest extends TestCase {
 	public void testTrue() throws Exception {
 		assertEquals(true, true);
 	}
+	
+	@Test
+	public void given2018Roman_WhenConvertingToArabic_ThenReturn2018() {
+	    String roman2018 = "MMXVIII";
+
+	    int result = RomanArabicConverter.romanToArabic(roman2018);
+
+	    assertThat(result).isEqualTo(2018);
+	}
 }
